119cebe2b709259984f3d661fea7c8c9a5913ae0
Gaurav Shah <gshah2@eb2-2236-aw01.csc.ncsu.edu>
2015-11-05 21:02:31 -0500

    Debugged to 4 errors

--- scheduler.cu
+++ scheduler.cu
@@ -5,7 +5,7 @@
 
 unsigned int jobChunkArray[1000];
 int jobChunkCounter = 0;
-int SMC_workerCount[] = {0, 0, 0, 0, 0, 0}; // Array of counter for blocks created for each SM.
+unsigned int SMC_workerCount[] = {0, 0, 0, 0, 0, 0}; // Array of counter for blocks created for each SM.
 
 // __device__ __host__ void square(long int * d_array_in, long int * d_array_out, int length)//, unsigned int * __SMC_chunkCount, unsigned int * __SMC_newChunkSeq, unsigned int __SMC_chunksPerSM)
 // {
@@ -22,36 +22,36 @@ int SMC_workerCount[] = {0, 0, 0, 0, 0, 0}; // Array of counter for blocks creat
 //     // __SMC_End
 // }
 
-#define __SMC_init  \
-unsigned int __SMC_workersNeeded = 2; \ //__SMC_numNeeded();  // Need to be made dynamic based on user input.
-unsigned int * __SMC_newChunkSeq = jobChunkArray;  \
-unsigned int * __SMC_workerCount = SMC_workerCount; // Array of counter for blocks created for each SM.
+// #define __SMC_init  \
+// int __SMC_workersNeeded = 2; \ //__SMC_numNeeded();  // Need to be made dynamic based on user input.
+// int * __SMC_newChunkSeq = jobChunkArray;  \
+// int * __SMC_workerCount = SMC_workerCount; // Array of counter for blocks created for each SM.
 
-#define __SMC_Begin  \
-__shared int __SMC_workingCTAs;  \
-__SMC_getSMid;  \
-if (offsetInCTA == 0)  \
-    __SMC_workingCTAs = atomicInc(&__SMC_workerCount[__SMC_smid], INT_MAX);  \
-__syncthreads();  \
-if (__SMCS_workingCTAs >= __SMC_workersNeeded)  \
-    return;  \
-int __SMC_chunksPerCTA = \
-    __SMC_chunksPerSM / __SMC_workersNeeded; \
-int __SMC_startChunkIDidx = __SMC_smid * __SMC_chunksPerSM + \
-    __SMC_workingCTAs * __SMC_chunksPerCTA;  \
-for (int __SMC_chunkIDidx = __SMC_startChunkIDidx; \
-    __SMC_chunkIDidx < __SMC_startChunkIDidx + \
-        __SMC_chunksPerCTA; \
-    __SMC_chunkDidx ++) {  \
-    __SMC_chunkID = __SMC_newChunkSeq[__SMC_chunkIDidx]};
+// #define __SMC_Begin  \
+// __shared int __SMC_workingCTAs;  \
+// __SMC_getSMid;  \
+// if (offsetInCTA == 0)  \
+//     __SMC_workingCTAs = atomicInc(&__SMC_workerCount[__SMC_smid], INT_MAX);  \
+// __syncthreads();  \
+// if (__SMCS_workingCTAs >= __SMC_workersNeeded)  \
+//     return;  \
+// int __SMC_chunksPerCTA = \
+//     __SMC_chunksPerSM / __SMC_workersNeeded; \
+// int __SMC_startChunkIDidx = __SMC_smid * __SMC_chunksPerSM + \
+//     __SMC_workingCTAs * __SMC_chunksPerCTA;  \
+// for (int __SMC_chunkIDidx = __SMC_startChunkIDidx; \
+//     __SMC_chunkIDidx < __SMC_startChunkIDidx + \
+//         __SMC_chunksPerCTA; \
+//     __SMC_chunkDidx ++) {  \
+//     __SMC_chunkID = __SMC_newChunkSeq[__SMC_chunkIDidx]};
 
-#define __SMC_End  }
+// #define __SMC_End  }
 
-#define __SMC_getSMid  \
-uint __SMC_smid;  \
- asm("mov.u32 %0, %smid;" : "=r"(__SMC_smid) );
+// #define __SMC_getSMid  \
+// uint __SMC_smid;  \
+//  asm("mov.u32 %0, %smid;" : "=r"(__SMC_smid) );
 
-__host__ __device__ void square(int *array, int threadIdx)
+__host__ __device__ void * square(int *array, int threadIdx)
 {
 	// __SMC_Begin
 	//printf("The value of x is %d \n",x);
@@ -64,10 +64,11 @@ __host__ __device__ void square(int *array, int threadIdx)
 	//	return;
     int f = array[threadIdx];
     array[threadIdx] = f * f;
+    return (void *)array;
     // __SMC_End
 }
 
-void* (*func)();
+typedef void* (*func)();
 
 typedef struct bag_elem
 {
@@ -86,31 +87,43 @@ int length_of_chunk = 6;
 
 int taskAdd(void* (*func)(void*), void* arg, int sm)
 {
-	Bag[sm_index[sm]][sm].func = square;
-	Bag[sm_index[sm]][sm].arg = arg;
-	Bag[sm_index[sm]][sm].size_of_chunk = length;
+	(Bag[sm_index[sm]][sm]).y = square;
+	(Bag[sm_index[sm]][sm]).arg = arg;
 	int retval = jobchunk_id;
 	jobchunk_id++;
-	jobChunkArray[jobChunkCounter] = arg;
+	jobChunkArray[jobChunkCounter] = retval; // Doubtful.
 	jobChunkCounter++;
 	return retval;
 }
 
 
-__global__ persistent_func(Bag_elem* Bag, unsigned int * __SMC_chunkCount, unsigned int * __SMC_newChunkSeq, int __SMC_chunksPerSM)
+__global__ void persistent_func(Bag_elem* Bag, unsigned int * __SMC_chunkCount, unsigned int * __SMC_newChunkSeq, unsigned int __SMC_chunksPerSM)
 {
-	__SMC_Begin;
+	__shared__ int __SMC_workingCTAs;
+	int __SMC_chunkID;
+	uint __SMC_smid;  \
+ 	asm("mov.u32 %0, %smid;" : "=r"(__SMC_smid) );
+	//if (offsetInCTA == 0)
+    __SMC_workingCTAs = atomicInc(&__SMC_chunkCount[__SMC_smid], INT_MAX);
+	__syncthreads(); 
+	if (__SMC_workingCTAs >= __SMC_chunksPerSM)
+    return;
+	int __SMC_chunksPerCTA = __SMC_chunksPerSM / __SMC_chunksPerSM; 
+	int __SMC_startChunkIDidx = __SMC_smid * __SMC_chunksPerSM + __SMC_workingCTAs * __SMC_chunksPerCTA;
+	for (int __SMC_chunkIDidx = __SMC_startChunkIDidx; __SMC_chunkIDidx < __SMC_startChunkIDidx + __SMC_chunksPerCTA; __SMC_chunkIDidx ++) { 
+    __SMC_chunkID = __SMC_newChunkSeq[__SMC_chunkIDidx];
+
 	int x = threadIdx.x + __SMC_chunkID * blockDim.x;
 
-	Bag[__SMC_smid][x]->func(Bag[_SMC_smid][x]->arg, threadIdx.x);
-	__SMC_End;
+	Bag[__SMC_smid][x]->func(Bag[__SMC_smid][x]->arg, threadIdx.x);
+	
+	}
 
 
 }
 
 extern "C" void schedule(int n, int m)
 {
-
 	// allocating memory on cuda for cpu variables
 	Bag_elem* d_Bag;
 	cudaMalloc((void**) &d_Bag, sizeof(Bag_elem)*6*1000);
@@ -118,11 +131,22 @@ extern "C" void schedule(int n, int m)
 	// copying data from cpu to gpu
 	cudaMemcpy(d_Bag, Bag, sizeof(Bag_elem)*6*1000, cudaMemcpyHostToDevice);
 
-	__SMC_init;
+	unsigned int __SMC_workersNeeded = 2;  //__SMC_numNeeded();  // Need to be made dynamic based on user input.
+	unsigned int * __SMC_newChunkSeq = jobChunkArray;
+	unsigned int * __SMC_workerCount = SMC_workerCount; // Array of counter for blocks created for each SM.
 
 	persistent_func <<< n, m >>> (d_Bag, __SMC_workerCount, __SMC_newChunkSeq, __SMC_workersNeeded);
 
 	cudaMemcpyAsync(Bag, d_Bag, sizeof(Bag_elem)*6*1000, cudaMemcpyDeviceToHost);
+
+	int i, y;
+	for(i = 0; i < 6; i++)
+	{
+		for (y = 0; y < length_of_chunk; y++)
+			printf("%d ", (Bag[0][i].arg)[y]);
+		printf("\n");
+	}
+	cudaFree(d_Bag);
 
 }
 
